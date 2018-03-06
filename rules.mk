77b1677fe8e8b107234a21520acc1d325098830e
SurgeNight <surgenight@hotmail.com>
2018-03-07 00:00:20 +0800

    Initial commitl.

---
+++ rules.mk
@@ -0,0 +1,17 @@
+
+# QMK Build Options
+#   change to "no" to disable the options, or define them in the Makefile in 
+#   the appropriate keymap folder that will get included automatically
+#
+CONSOLE_ENABLE = no         # Enable debugging console
+COMMAND_ENABLE = no         # Commands for debug and configuration
+TAP_DANCE_ENABLE = no 		# Tap Dance skills
+UNICODE_ENABLE = no         # Unicode
+MOUSEKEY_ENABLE = yes       # control over cursor movements and clicks via keycodes/custom functions
+EXTRAKEY_ENABLE = yes       # use the system and audio control key codes
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+
